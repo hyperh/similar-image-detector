0a2bb00824171b600c814c6ee76cd0533847b037
Heyse Li <hyperh@users.noreply.github.com>
2018-06-19 23:26:37 -0400

    Refactor

--- get_similar_images_script.py
+++ get_similar_images_script.py
@@ -1,12 +1,13 @@
 import torch
 import data
 import distance
+import results
 
-folder = 'saves/2018-06-18-21-29-41.802806'
+folder = 'saves/2018-06-18-23-05-40.687803'
 model_path = '{}/trained_model.pt'.format(folder)
 img_size = (28, 28)
 autoencoder = torch.load(model_path)
-threshold = 1.00000e-01 * 1
+threshold = 5.00000e-02 * 1
 
 dataset, dataset_loader = data.get_data(
 	img_size=img_size, batch_size=1, shuffle=False)
@@ -16,3 +17,5 @@ encodeds = distance.get_encodeds(dataset, dataset_loader, autoencoder)
 encodeds_tensor = distance.get_encodeds_tensor(encodeds)
 all_distances = distance.calc_distances(encodeds, encodeds_tensor)
 similar_images = distance.find_similar_images(folder, img_paths, all_distances, threshold)
+
+results.save_results_html(folder, similar_images)
